cf6c2216f1656dc8aa237a1e08e523ec274e3166
jonathandavidblack <jonnybl915@gmail.com>
2016-08-04 16:06:22 -0400

    added camera functionality

--- app/src/main/java/com/jdblack/babaramironcam/MainActivity.java
+++ app/src/main/java/com/jdblack/babaramironcam/MainActivity.java
@@ -1,13 +1,58 @@
 package com.jdblack.babaramironcam;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SurfaceView surfaceView;
+        android.hardware.Camera camera;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+
+        camera = android.hardware.Camera.open();
+        surfaceView.getHolder().addCallback(this);
+        try {
+            camera.setPreviewDisplay(surfaceView.getHolder());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
